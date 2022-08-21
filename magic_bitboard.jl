9148f7c8be74cb3f01883cb000258d29794da598
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2022-08-21 08:44:01 +0700

    Update magic_bitboard.jl

--- magic_bitboard.jl
+++ magic_bitboard.jl
@@ -17,3 +17,90 @@ end
 function get_lookup_index(mask::UInt64, magic::UInt64, shift::Integer, query::UInt64)
     return ((query&mask)*magic)>>shift
 end
+
+struct DONTCARE end;
+
+
+
+#=
+
+Assumption: if it exists in mask, then it is used in at least one spot. 
+That is, there is some point where masking that bit is required.
+
+Only a subset needs to be searched.
+The number which overflows the last masked bit does not need to be searched. Pruned.
+Imagined
+00000...00000* magic = 00000...
+Now, for any number not equal 000...000
+There must be some bits in the magic bit corresponding. 
+
+With only 1 bit, and each with different number, must be different...
+however... yeah.
+
+TORD suggests a densely populated bit...
+
+For each r, it loops every 2^64/gcd(2^64, r)
+
+Its proof is simple. Let's say 10 and 4.
+It will go through 10/gcd(10,4) = 5 and then loop around.
+
+lcm(r1, r2, r3, ...) may reduce the size down.
+
+gcd(X::UInt64, big_interval) = X&-X
+proof: Last bit.
+
+
+
+U64 snoob (U64 x) {
+   U64 smallest, ripple, ones;
+   smallest = x & -x;
+   ripple = x + smallest;
+   ones = x ^ ripple;
+   ones = (ones >> 2) / smallest;
+   return ripple | ones;
+}
+
+1111100000000011100
+1111100000000100000
+ripple add.
+0000000000000111100
+ones, removing higher bits.
+0000000000000000011
+two ones gone.
+1111100000000100011
+Now, the rippled one and the bit before the ripple unset... then shifted back to the beginning.0.
+The rippled one definitely the last one.
+=#
+
+
+"
+returns the last number which needs to be checked.
+This loops from zero to that number. zero magic number, however, yields trivial result mapping all to the same value, and thus not need to be checked.
+"
+function magic_bitboard_range(mask)
+    #The last bit represents its 
+    Big_interval=Int128(2)^64
+    
+    return UInt64(div(Big_interval,(mask&-mask))-1)
+end
+
+
+
+
+
+function verify_magic_bitboard(mask::UInt64,magic::UInt64, shift, f::Function, return_type::Type)
+
+end
+"""
+The mask suggests all the combinations possible.
+f takes input as the bit representation of traits. This function should be constructed with helps of macros.
+f should return Union{return_type, DONTCARE}
+The DONTCARE suggests that the magic could do whatever it wants.
+
+"""
+function find_magic_bitboard(mask::UInt64, f::Function, return_type::Type = Any)
+    for i in search_space(mask)
+        Temp::Union{return_type, DONTCARE} = f(i)
+    end
+    
+end
