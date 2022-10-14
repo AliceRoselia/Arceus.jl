c3eb54ee57455209c916355b00fa83553184882a
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2022-10-14 20:46:32 +0700

    Update magic_bitboard.jl

--- magic_bitboard.jl
+++ magic_bitboard.jl
@@ -132,7 +132,7 @@ function get_new_guess(guess)
     error("working in progress.")
 end
 
-function find_magic_bitboard(mask::UInt64, f::Function, return_type::Type = Any, shift_minimum::Integer = 32)
+function find_magic_bitboard(mask::UInt64, f::Function, return_type::Type = Any, shift_minimum::Integer = 32, guess_limits::Integer=1000000)
     answer_table = Dict{UInt64, return_type}()
     #We get a hash table but not a perfect one so we need to do it again.
     for i in maskedBitsIterator(mask)
@@ -144,8 +144,7 @@ function find_magic_bitboard(mask::UInt64, f::Function, return_type::Type = Any,
     answer_table = collect(answer_table)
     #Now, we need to find the perfect hash that solves this answer table.
     guess = get_guess() #Can change if needed.
-    shift = get_shift(mask)
-    guess_limits = 1000000 # can change or make this a param later.
+    initial_shift = shift = get_shift(mask)
     limit = guess_limits
     while !(verify_magic_bitboard(answer_table, guess, shift, return_type))
         guess = get_new_guess(guess)
@@ -158,6 +157,25 @@ function find_magic_bitboard(mask::UInt64, f::Function, return_type::Type = Any,
             limit = guess_limits
         end
     end
+    if (shift == initial_shift)
+        trying_to_shrink = true
+        while trying_to_shrink
+            shift += 1
+            if (shift >= 64)
+                break
+            end
+            limit = guess_limits
+            while !(verify_magic_bitboard(answer_table, guess, shift, return_type))
+                guess = get_new_guess(guess)
+                limit -= 1
+                if (limit <= 0)
+                    trying_to_shrink = false
+                    break
+                end
+            end
+        end
+    end
+
     return guess, shift
     #You can construct later.
 end
