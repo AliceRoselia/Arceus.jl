243b7871ebd2fd155cb4840bdf91dfea9d022957
AliceRoselia <63040919+AliceRoselia@users.noreply.github.com>
2023-06-19 21:20:36 +0700

    Updating vision.

--- Arceus.jl
+++ Arceus.jl
@@ -1,10 +1,10 @@
 
 #module Arceus
     include("magic_bitboard.jl")
-    include("bits_allocation.jl")
-    include("trait_pool.jl")
-    include("trait_modification.jl")
-    include("trait_test.jl")
-    include("subpool.jl")
+    include("bits_allocation.jl") 
+    include("trait_pool.jl") 
+    include("trait_modification.jl") 
+    include("trait_test.jl") 
+    include("subpool.jl") 
     include("clear_context.jl")
 #end
